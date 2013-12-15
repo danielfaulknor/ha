82c98eba84dc4c61b4c170aba82b9cc217554c1b
Dan Faulknor <daniel@zzrv.co>
2013-12-15 19:30:50 +1100

    Adding processing modules

--- bin/modules.sh
+++ bin/modules.sh
@@ -2,4 +2,6 @@
 APP_PATH=/root/ha
 $APP_PATH/modules/433mhz/mqtt_2_433mhz.py &
 $APP_PATH/modules/433mhz/433mhz_2_mqtt.py &
+$APP_PATH/modules/processing/sensors.py &
+$APP_PATH/modules/processing/actuators.py &
 bash
