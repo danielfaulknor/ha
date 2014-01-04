a96dcaec59091b59e8c926db14557e4b77897480
Dan Faulknor <daniel@zzrv.co>
2014-01-04 16:25:17 +1100

    Moving modules out into separate git repos

--- modules/processing/codes_example.py
+++
@@ -1,7 +0,0 @@
-def sensor_SomeDoor():
-        print "Some Door Opened"
-        mqttc.publish("actuators", json.dumps(["switch_HallLight", "on"]))
-
-rf_433mhz = {
-        "0x471d1e" : sensor_SomeDoor,
-}
